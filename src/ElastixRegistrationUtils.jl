195b575bbf1da415ebca6d30fbde7d92e2c3b273
Donghoon Lee <mdhe1248@gmail.com>
2023-04-09 11:25:05 -0400

    Package dependency BrainAnnotationMapping

--- src/ElastixRegistrationUtils.jl
+++ src/ElastixRegistrationUtils.jl
@@ -1,5 +1,5 @@
 module ElastixRegistrationUtils
-using DelimitedFiles, Images, JSON
+using DelimitedFiles, Images, JSON, BrainAnnotationMapping
 
 export elastix_inverse_tform, modify_first_txt, load_points, prepare_directories, prepare_filenames, load_annotation_lbls
 
